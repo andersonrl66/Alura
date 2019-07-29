4bc961ea58712dba1acd17fb0b3cd94ccbd4c082
Anderson Lemos <andersonrl66@gmail.com>
2019-07-29 18:02:10 -0300

    Atividade 3

--- jpa/financas/src/br/com/caelum/financas/teste/TesteConta.java
+++ jpa/financas/src/br/com/caelum/financas/teste/TesteConta.java
@@ -1,10 +1,8 @@
 package br.com.caelum.financas.teste;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
 
 public class TesteConta {
 
@@ -16,16 +14,13 @@ public class TesteConta {
         conta.setAgencia("123");
         conta.setNumero("456");
         
-        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("financas");
-        EntityManager em = emf.createEntityManager();
-
+        EntityManager em = new JPAUtil().getEntityManager();
 
         em.getTransaction().begin();
         em.persist(conta);
         em.getTransaction().commit();
 
         em.close();
-        emf.close();
 
     }
     
