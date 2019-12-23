13074930b626c522b93eef50f7db681f7c16410c
Anderson Lemos <andersonrl66@gmail.com>
2019-12-23 18:14:59 -0300

    es6 parte 2 parcial

--- es6/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ es6/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,20 +1,18 @@
 class ListaNegociacoes {
 
     constructor() {
-        this._negociacoes = []
+  
+          this._negociacoes = [];
     }
-
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
     }
-    
+  
     get negociacoes() {
-
         return [].concat(this._negociacoes);
     }
-
-    esvazia()   {
-
+  
+    esvazia() {
         this._negociacoes = [];
     }
-}
+  }
